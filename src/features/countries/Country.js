d9e7489f12d56041c41b9426d834a374bf2542a7
kambereBr <brunokambere@gmail.com>
2023-09-08 21:20:35 +0200

    create Country component and style it

---
+++ src/features/countries/Country.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { FaRegArrowAltCircleRight } from 'react-icons/fa';
+import '../../assets/css/Country.css';
+
+const Country = ({
+  iso3, country, code, populationCounts, classValue,
+}) => (
+  <li className={`country-card ${classValue}`} id={iso3}>
+    <Link to={`/details/${iso3}`}><FaRegArrowAltCircleRight className="country-card-row" /></Link>
+    <div className="country-card-row-content">
+      <span className="country-description">{code}</span>
+      <span className="country-description">{country}</span>
+      <span className="cities-number">{populationCounts}</span>
+    </div>
+  </li>
+);
+
+Country.propTypes = {
+  iso3: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  code: PropTypes.string.isRequired,
+  populationCounts: PropTypes.number.isRequired,
+  classValue: PropTypes.string.isRequired,
+};
+
+export default Country;
